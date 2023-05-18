f14278a80a683acd64ddc765aa3793a651d1531d
MIGUEL CARLOS BERTOLDI <miguel_c_bertoldi@estudante.sesisenai.org.br>
2023-05-18 09:41:17 -0300

    .

--- src/todo/todo.component.ts
+++ src/todo/todo.component.ts
@@ -1,9 +1,63 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+
+
+interface Tarefa {
+    nome: string,
+    categoria: string
+}
 
 @Component({
-    template: '<h1>Olá</h1>'
+    templateUrl: './todo.component.html',
+    styleUrls: ['./todo.component.css']
 })
 
 export class TodoComponent {
+    listaCategorias: string[] = [];
+    tarefas: Tarefa[] = [];
+    categoria: string = '';
+    nomeTarefa: string = '';
+    categoriaAlt: string = '';
+
+    listaTarefas: Tarefa[] = [];
+
+    ngOnInit() {
+
+        if (localStorage.getItem('listaTarefas') != null) {
+            this.listaTarefas = JSON.parse(localStorage.getItem('listaTarefas'));
+        }
+
+        if (localStorage.getItem('listaCategorias') != null) {
+            this.listaCategorias = JSON.parse(localStorage.getItem('listaCategorias'));
+        }
+    }
+
+    cadastrarTarefa(): void {
+
+        const tarefaAdd: Tarefa = {
+            nome: this.nomeTarefa,
+            categoria: this.categoria
+          }
+
+          if (tarefaAdd.nome != '' && tarefaAdd.categoria != '') {
+
+              this.listaTarefas.push(tarefaAdd);
+              localStorage.setItem('listaTarefas', JSON.stringify(this.listaTarefas));  
+            }
+        this.limparInput();
+    }
+
+    removerTarefa(tarefa): void {
+        this.listaTarefas.splice(this.listaTarefas.indexOf(tarefa), 1);
+        localStorage.setItem('listaTarefas', JSON.stringify(this.listaTarefas));
+    }
+
+    alterarCategoria(tarefaClick): void {
+        tarefaClick.categoria = this.categoriaAlt;
+    }
+
+    limparInput(): void {
+        this.nomeTarefa = '';
+        this.categoria = '';
+    }
 
 }
