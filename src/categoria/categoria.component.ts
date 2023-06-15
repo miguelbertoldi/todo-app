4f59aac754b94af5b78a05c39db2f30a7da55fbc
MIGUEL CARLOS BERTOLDI <miguel_c_bertoldi@estudante.sesisenai.org.br>
2023-06-15 16:41:36 -0300

    .

--- src/categoria/categoria.component.ts
+++ src/categoria/categoria.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 
-
+interface Propriedade {
+  nome: string,
+  tipo: string,
+  conteudo: string[] | string | number,
+  inputAdd: boolean
+}
 
 @Component({
   templateUrl: './categoria.component.html',
@@ -9,11 +14,56 @@ import { Component, OnInit } from "@angular/core";
 
 export class CategoriaComponent {
 
-
+  nome: string;
+  tipo: string;
+  conteudo: string | number;
+  listaTipos: string[] = ['Texto', 'Número', 'Seleção'];
+  listaProps: Propriedade[] = [];
 
   ngOnInit() {
+    if (localStorage.getItem('listaProps') != null) {
+      this.listaProps = JSON.parse(localStorage.getItem('listaProps')); 
+    }
+  }
+
+  cadastrarPropriedade (): void {
+
+      const newProp: Propriedade = {
+        nome: this.nome,
+        tipo: this.tipo,
+        conteudo: this.conteudo,
+        inputAdd: false
+      }
+
+
+    this.listaProps.push(newProp);
+    this.localStorage();
+  }
+
+  removerPropriedade (prop: Propriedade): void {
+    this.listaProps.splice(this.listaProps.indexOf(prop), 1);
+    this.localStorage();
+  }
+
+  adicionarItem (prop: Propriedade): void {
+
+      prop.conteudo.push(this.conteudo);
+      prop.inputAdd = !prop.inputAdd;
+      this.localStorage();
 
   }
+
+  mostraInput (prop: Propriedade): void {
+    prop.inputAdd = !prop.inputAdd;
+  }
+
+  localStorage (): void {
+    localStorage.setItem('listaProps', JSON.stringify(this.listaProps));
+  }
+
+
+
+
 
 
 }
